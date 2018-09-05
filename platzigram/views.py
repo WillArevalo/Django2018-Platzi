d1382bf9d573be561c0fd4d798f9ac59b295f96c
Unknown <willi11-95@hotmail.com>
2018-09-05 16:25:31 -0500

    Pasando argumentos por la URL

--- platzigram/views.py
+++ platzigram/views.py
@@ -1,17 +1,38 @@
 from django.http import HttpResponse
 from datetime import datetime
-from numpy import sort
+import json
 
 def hello_world(request):
 	"Return a Gretting."
 	now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
-	return HttpResponse('Oh, hi! Current server time is {}'.format(now))
+	return HttpResponse(
+		'Oh, hi! Current server time is {}'.format(now)
+	)
 
-def hi(request):
-	"Hi."
+def sorted(request):
+	"sorted."
 	#import pdb; pdb.set_trace()   Debugger
 	numbers = request.GET['numbers'].split(',')
 	numbers = [int(i) for i in numbers]
-	numbers = sort(numbers)
-	return HttpResponse('{}\n'.format(x) for x in numbers)
+	numbers = sorted(numbers)
+	data = {
+		'status':'ok',
+		'numbers':numbers,
+		'message':'Integers sorted successfully.'
+	}
+	return HttpResponse(
+		json.dumps(data, indent=4), 
+		content_type='application/json'
+	)
+def say_hi(request, name, age):
+	"Say Hi."
+	if age < 12:
+		message = 'Sorry {name}, you aren\'t allowed here'.format(
+			name = name
+		)
+	else:
+		message = 'Hello {name}, Welcome to Platzigram!'.format(
+			name = name
+		)
+	return HttpResponse(message)
 
